58ad4608df405b74379c41e60d799462a194ef5b
yunsu <dnlpys9999@gmail.com>
2024-04-19 00:58:18 +0000

    modify logger.py

--- logger.py
+++ logger.py
@@ -2,17 +2,19 @@ import json
 
 class Logger:
     def __init__(self) -> None:
-        self.dic_li = dict()
+        self.dic_li = []
+
+    def logging(self,question, answer):
+        self.dic_li.append({})
+        self.dic_li[-1]["question"] = question
+        self.dic_li[-1]["answer"] = answer
+        # print(self.dic_li)
 
     def save_txt(self):
         # dic_li = [ {'question':input(), 'answer':'2222'}, {'question':'3333', 'answer':'4444'}]
 
         with open('log_data.txt', 'w') as f:
             for item in self.dic_li:
-                f.write(json.dumps(item) + '\n')
-
-    def logging(self,question, answer):
-        self.dic_li["question"] = question
-        self.dic_li["answer"] = answer
-        
+                f.write(json.dumps(item, ensure_ascii=False) + '\n')
+        # print(self.dic_li)
       
